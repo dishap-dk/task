2e7022238d9cdcd81142c5d748ec9eec9ed895c0
dishap-dk <dishapushpad4@gmail.com>
2023-04-02 14:15:18 +0530

    host

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+const hostname="0.0.0.0"
 app.get("/", function (req, res) {
     res.send("node is running");
 });
 app.use("/", routes);
-    var Server = app.listen(process.env.PORT || 8080, function () {
+    var Server = app.listen( 8080,hostname, function () {
         var port = Server.address().port;
         console.log("App listening on port %s", port);
     });
