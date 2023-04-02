017972538075d4debf6bc5965e0bb671b1adbd79
dishap-dk <dishapushpad4@gmail.com>
2023-04-02 15:19:42 +0530

    html file added

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const hostname="0.0.0.0"
-app.get("/", function (req, res) {
-    res.send("node is running");
-});
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html", { root: __dirname });
+  });
+
 app.use("/", routes);
     var Server = app.listen( 8080,hostname, function () {
         var port = Server.address().port;
